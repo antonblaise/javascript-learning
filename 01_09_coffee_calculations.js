81ae76c04f5ef2c4075dcb577b6f6e3ab29e6193
antonblaise <antoniusblaise@gmail.com>
2024-10-22 22:15:47 +0800

    update 01_09_coffee_calculations.js and 01_10_food_truck.html

---
+++ 01_09_coffee_calculations.js
@@ -0,0 +1,12 @@
+// Calculate the total bill of coffees using array reduction and accumulator.
+
+function coffeeDate(coffeeArr) {
+    let coffeeSum = coffeeArr.reduce(
+        (totalCoffees, numCoffees) => (totalCoffees += numCoffees)
+    );
+
+    return `The total bill is $${coffeeSum * 1.25}.`;
+}
+
+console.log(coffeeDate([2, 5, 7, 1, 4]))
+
